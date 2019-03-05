3a29669a17b0a59776e44c05b6415a0fa8705771
Samaritan <30967204+samar1tan@users.noreply.github.com>
2019-03-05 09:20:01 +0800

    Update ThreadSum.java

--- main_course/programming_implementations/multithreading_demo/ThreadSum.java
+++ main_course/programming_implementations/multithreading_demo/ThreadSum.java
@@ -3,7 +3,7 @@ class ThreadSum extends Thread {
     private int upper_bound;
     private ShareVal sum;
 
-    public ThreadSum (int upper_bound, ShareVal sum) {
+    ThreadSum (int upper_bound, ShareVal sum) {
         this.upper_bound = upper_bound;
         this.sum = sum;
     }
